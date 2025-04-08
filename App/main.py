db15fcdd4156acf267855f089a30bde1eaa2bb99
sankalpd-optimus <sankalp.datta@optimusinfo.com>
2025-04-08 14:30:46 +0530

    allow_origins change

--- App/main.py
+++ App/main.py
@@ -26,7 +26,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # React server
+    allow_origins=["*"],  # React server
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
